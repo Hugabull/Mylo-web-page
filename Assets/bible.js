ff3e0e0ab5d5c4a9802f8db6b3a62b3f3a78e8c0
Marshall Dunn <marshall506@icloud.com>
2026-01-22 20:30:24 -0400

    added moving bible and photos to collection

---
+++ Assets/bible.js
@@ -0,0 +1,12 @@
+const img = document.getElementById("bible");
+
+  img.addEventListener("mouseenter", () => {
+    const maxX = window.innerWidth - img.offsetWidth;
+    const maxY = window.innerHeight - img.offsetHeight;
+
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
+
+    img.style.left = `${x}px`;
+    img.style.top = `${y}px`;
+  });
